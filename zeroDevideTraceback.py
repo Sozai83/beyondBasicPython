dbd940ccb4bb4c1ff2cf555209f076e60b9b34a8
Souzai83 <chikushi3.227@gmail.com>
2024-08-24 15:46:06 +1000

    Debugger test 02

---
+++ zeroDevideTraceback.py
@@ -0,0 +1,10 @@
+def spam(num1, num2):
+    return num1 / (num2 - 42)
+
+# spam(101,42) Returns zeroDevision error
+
+#print('Hello
+# Error indicates that it is in the following line - print('How are you?')
+print('How are you?')
+
+# Reason: Python interpreter did not notice the syntax error until it read the second line.
